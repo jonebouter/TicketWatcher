b74e6d8734771205d35e46e31300a69a5cb43979
jonebouter <jonebouter@gmail.com>
2026-07-20 12:14:47 +0200

    Update flight_checker.py

--- flight_checker.py
+++ flight_checker.py
@@ -1,27 +1,54 @@
 import os
+import requests
 
-MAX_PRICE = 600
+SERPAPI_KEY = os.getenv("SERPAPI_KEY")
+
 
 def check_flights():
-    # Tijdelijke testvlucht
-    # Later vervangen we dit gedeelte door een echte vlucht-API
 
-    flight = {
-        "route": "AMS → CUR",
-        "date": "14 augustus - 25 augustus",
-        "price": 499,
-        "link": "https://www.google.com/travel/flights"
+    params = {
+        "engine": "google_flights",
+        "departure_id": "AMS",
+        "arrival_id": "CUR",
+        "outbound_date": "2026-08-14",
+        "return_date": "2026-08-25",
+        "currency": "EUR",
+        "hl": "nl",
+        "gl": "nl",
+        "adults": 1,
+        "children": 0,
+        "api_key": SERPAPI_KEY
     }
 
-    if flight["price"] <= MAX_PRICE:
+    response = requests.get(
+        "https://serpapi.com/search",
+        params=params
+    )
+
+    data = response.json()
+
+    if "best_flights" not in data:
         return {
-            "found": True,
-            "route": flight["route"],
-            "date": flight["date"],
-            "price": flight["price"],
-            "link": flight["link"]
+            "found": False
         }
 
+
+    vlucht = data["best_flights"][0]
+
+    prijs = vlucht["price"]
+
+    airline = vlucht["flights"][0]["airline"]
+
+    vertrek = vlucht["flights"][0]["departure_airport"]["time"]
+
+    aankomst = vlucht["flights"][0]["arrival_airport"]["time"]
+
+
     return {
-        "found": False
+        "found": True,
+        "route": "AMS → CUR",
+        "date": f"{vertrek} - {aankomst}",
+        "price": prijs,
+        "airline": airline,
+        "link": data["search_metadata"]["google_flights_url"]
     }
